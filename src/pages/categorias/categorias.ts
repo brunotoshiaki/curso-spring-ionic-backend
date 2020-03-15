aa090a104bf6a0a54b409746ebb32db0f9bf2d17
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2020-03-15 19:44:51 -0300

    Carregando produtos de uma dada categoria

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -4,12 +4,7 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 import { CategoriaDTO } from '../../models/categoria.dto';
 import { API_CONFIG } from '../../config/api.config';
 
-/**
- * Generated class for the CategoriasPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @IonicPage()
 @Component({
@@ -22,7 +17,7 @@ export class CategoriasPage {
   items: CategoriaDTO[];
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
     public categoriaService: CategoriaService) {
   }
@@ -30,13 +25,14 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.findAll()
       .subscribe(response => {
-       this.items = response;
+        this.items = response;
 
       },
-      error => {});
+        error => { });
   }
 
-  showProdutos(){
-    this.navCtrl.push('ProdutosPage');
+  showProdutos(categoria_id: string) {
+                                      // nome, parametro do metodo
+    this.navCtrl.push('ProdutosPage',{categoria_id: categoria_id});
   }
 }
