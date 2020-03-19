8ece3c00c8901bb431b3f285c67da694961c5b83
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2020-03-18 22:36:52 -0300

    Terminando as funcionalidades do carrinho

--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -30,9 +30,63 @@ export class CartService {
         let position = cart.items.findIndex(x => x.produto.id == produto.id);
         // nao existe 
         if (position == -1) {
+            //adiciona o intem
             cart.items.push({ quantidade: 1, produto: produto });
         }
         this.storage.setCart(cart);
         return cart;
     }
+
+    removeProduto(produto: ProdutoDTO): Cart {
+        let cart = this.getCart();
+        //encontra um elemento x tal que o x.produto.id = produto.id
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        // se ele for encontrador
+        if (position != -1) {
+            //remove o intem
+            cart.items.splice(position, 1);
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    increaseQuantity(produto: ProdutoDTO): Cart {
+        let cart = this.getCart();
+        //encontra um elemento x tal que o x.produto.id = produto.id
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        // se ele for encontrador
+        if (position != -1) {
+            cart.items[position].quantidade++;
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    decreaseQuantity(produto: ProdutoDTO): Cart {
+        let cart = this.getCart();
+        //encontra um elemento x tal que o x.produto.id = produto.id
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+        // se ele for encontrador
+        if (position != -1) {
+            cart.items[position].quantidade--;
+            if (cart.items[position].quantidade < 1) {
+                cart = this.removeProduto(produto);
+            }
+        }
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    total(): number {
+        let cart = this.getCart();
+        let soma = 0;
+        for (var i = 0; i < cart.items.length; i++) {
+            soma += cart.items[i].produto.preco * cart.items[i].quantidade;
+        }
+
+        return soma;
+    }
+
+
+
 }
