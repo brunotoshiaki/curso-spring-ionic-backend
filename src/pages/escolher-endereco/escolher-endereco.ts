97ca1f9f4c348e4b174cf7723692ca9523338aac
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2020-03-21 13:04:54 -0300

    Tela de escolha de pagamento

--- src/pages/escolher-endereco/escolher-endereco.ts
+++ src/pages/escolher-endereco/escolher-endereco.ts
@@ -52,8 +52,9 @@ export class EscolherEnderecoPage {
   }
 
   nextPage(item: EnderecoDTO) {
-    this.pedido.enderecoDeEntrega = { id: item.id };
-    console.log(this.pedido);
+    this.pedido.enderecoDeEntrega = {id: item.id};
+    console.log(this.pedido); 
+    this.navCtrl.push('PagamentoPage', {pedido: this.pedido});
   }
 
 }
