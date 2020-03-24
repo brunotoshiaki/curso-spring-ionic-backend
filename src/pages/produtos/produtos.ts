3529c2a6f5355b13f09c7b31a190771e36bd0ba2
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2020-03-23 21:32:10 -0300

    Infinite scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -12,7 +12,9 @@ import { API_CONFIG } from '../../config/api.config';
   templateUrl: 'produtos.html',
 })
 export class ProdutosPage {
-  items: ProdutoDTO[];
+  items: ProdutoDTO[] = [];
+
+  page: number = 0;
 
   constructor(
     public navCtrl: NavController,
@@ -28,19 +30,24 @@ export class ProdutosPage {
   carregarDados() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id)
+    this.produtoService.findByCategoria(categoria_id, this.page, 10)
       .subscribe(response => {
-        this.items = response['content'];
+        let inicio = this.items.length;
+        //concatena o item com a resposta
+        this.items = this.items.concat(response['content']);
+        let fim = this.items.length - 1;
         loader.dismiss();
-        this.loadImageUrls();
+        console.log(this.page);
+        console.log(this.items);
+        this.loadImageUrls(inicio, fim);
       },
         error => {
           loader.dismiss();
         });
   }
 
-  loadImageUrls() {
-    for (var i = 0; i < this.items.length; i++) {
+  loadImageUrls(inicio: number, fim: number) {
+    for (var i = inicio; i <= fim; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
@@ -65,10 +72,19 @@ export class ProdutosPage {
   }
 
   doRefresh(refresher) {
-    this.carregarDados()
+    this.page = 0;
+    this.items = [];
+    this.carregarDados();
     setTimeout(() => {
-
       refresher.complete();
+    }, 1000);
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page++;
+    this.carregarDados();
+    setTimeout(() => {
+      infiniteScroll.complete();
     }, 1000);
   }
 }
