fe4718aeba571dc9200b17d4c102a60348937686
Bruno Toshiaki <brunotoshiaki@hotmail.com>
2020-06-21 18:24:33 -0300

    Correções da galeria e camera

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,29 +1,32 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { StorageService } from '../../services/storage.service';
-import { ClienteDTO } from '../../models/cliente.dto';
-import { ClienteService } from '../../services/domain/cliente.service';
-import { API_CONFIG } from '../../config/api.config';
-import { Camera} from '@ionic-native/camera';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { StorageService } from "../../services/storage.service";
+import { ClienteDTO } from "../../models/cliente.dto";
+import { ClienteService } from "../../services/domain/cliente.service";
+import { API_CONFIG } from "../../config/api.config";
+import { Camera } from "@ionic-native/camera";
+import { DomSanitizer } from "@angular/platform-browser";
 
 @IonicPage()
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
+  selector: "page-profile",
+  templateUrl: "profile.html",
 })
 export class ProfilePage {
-
   cliente: ClienteDTO;
   picture: string;
   cameraOn: boolean = false;
-  options:any;
+  options: any;
+  profileImage:any;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
     public clienteService: ClienteService,
-    public camera: Camera) {
+    public camera: Camera,
+    public sanitizer: DomSanitizer ) {
+    this.profileImage = "assets/imgs/avatar-blank.png";
   }
 
   ionViewDidLoad() {
@@ -38,76 +41,92 @@ export class ProfilePage {
           this.cliente = response as ClienteDTO;
           this.getImageIfExists();
         },
-          error => {
-            if (error.status == 403) {
-              this.navCtrl.setRoot('HomePage');
-            }
-          })
-    } else {
-      this.navCtrl.setRoot('HomePage');
+        error => {
+          if (error.status == 403) {
+            this.navCtrl.setRoot('HomePage');
+          }
+        });
     }
+    else {
+      this.navCtrl.setRoot('HomePage');
+    }    
   }
 
   getImageIfExists() {
     this.clienteService.getImageFromBucket(this.cliente.id)
-      .subscribe(response => {
-        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
-      },
-        error => { });
+    .subscribe(response => {
+      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      this.blobToDataURL(response).then(dataUrl => {
+        let str : string = dataUrl as string;
+        this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+      });
+    },
+    error => {
+      this.profileImage = 'assets/imgs/avatar-blank.png';
+    });
   }
 
-  
+  // https://gist.github.com/frumbert/3bf7a68ffa2ba59061bdcfc016add9ee
+  blobToDataURL(blob) {
+    return new Promise((fulfill, reject) => {
+        let reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = (e) => fulfill(reader.result);
+        reader.readAsDataURL(blob);
+    })
+  }
 
   getCameraPicture() {
-
     this.cameraOn = true;
 
-    this.options =   {
+    this.options = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.PNG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
+      mediaType: this.camera.MediaType.PICTURE,
+    };
 
     this.camera.getPicture(this.options).then((imageData) => {
       this.picture = 'data:image/png;base64,' + imageData;
       this.cameraOn = false;
-    }, (err) => {
-    });
+     }, (err) => {
+       this.cameraOn = false;
+     });
   }
 
   getGalleryPicture() {
-
     this.cameraOn = true;
 
-    this.options =   {
+    this.options = {
       quality: 100,
       sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.PNG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
+      mediaType: this.camera.MediaType.PICTURE,
+    };
 
-    this.camera.getPicture(this.options).then((imageData) => {
-     this.picture = 'data:image/png;base64,' + imageData;
-     this.cameraOn = false;
-    }, (err) => {
-    });
+    this.camera.getPicture(this.options).then(
+      (imageData) => {
+        this.picture = "data:image/png;base64," + imageData;
+        this.cameraOn = false;
+      },
+      (err) => {
+        this.cameraOn = false;
+      }
+    );
   }
 
   sendPicture() {
-    this.clienteService.uploadPicture(this.picture)
-      .subscribe(response => {
+    this.clienteService.uploadPicture(this.picture).subscribe(
+      (response) => {
         this.picture = null;
-        this.carregarDados();
+        this.getImageIfExists();
       },
-        error => {
-
-        })
+      (error) => {}
+    );
   }
 
   descartarImagem() {
     this.picture = null;
   }
-
 }
